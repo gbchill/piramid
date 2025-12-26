bc346cdfaff2d40bd13244dbc95690466b8ae16b
ashworks1706 <asriv132@asu.edu>
2025-12-26 19:34:57 +0530

    added load and get

---
+++ rusty-kv/src/main.rs
@@ -0,0 +1,49 @@
+use std::env;
+use rusty_kv::RustyKV;
+
+fn main(){
+    // collect command line arguments into a vector, rust gives us std::env::args to use it 
+     let args: Vec<String> = env::args().collect();
+
+    let mut store = RustyKV::open("database.db").expect("Failed to open DB");
+
+    if args.len() < 2{
+        println!("Usage : ");
+        println!("  cargo run set <key> <value>");
+        println!("  cargo run get <key>");
+        return;
+    }
+
+    let command = &args[1];
+
+    match command.as_str(){
+        "set" => {
+            if args.len() < 4{
+                println!("Error : 'set' requires a key and a value");
+                return;
+            }
+            let key = args[2].clone();
+            let value = args[3].clone();
+
+            store.set(key, value).expect("Failed to set data");
+            println!("Ok")
+        },
+        "get" =>{
+            if args.len() < 3 {
+                println!("Error get requires key!!!!");
+                return;
+            }
+            let key = args[2].clone();
+
+            match store.get(key){
+                Ok(Some(value)) => println!("{}",value),
+                Ok(None) => println!("Key not found"),
+                Err(e) => println!("Error is {}", e),
+            }
+        },
+        _ =>{
+            println("Unknown command : {}",command);
+        }
+    }
+
+}
